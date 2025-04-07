c514ed9fa9afda4520640f18055b2128d4d434f1
gustavopalaciosc <gpalaciosc@uc.cl>
2025-04-07 14:37:11 -0400

    Agregada política greedy para iterative policy evaluation.

--- MDPs/Algorithms/IterativePolicyEvaluation.py
+++ MDPs/Algorithms/IterativePolicyEvaluation.py
@@ -1,3 +1,4 @@
+from time import sleep
 
 class IterativePolicyIteration:
     def __init__(self, gamma, problem, threshold =  0.0000000001):
@@ -30,5 +31,57 @@ class IterativePolicyIteration:
 
     def get_initial_state_value(self):
         return self.v_values[self.problem.get_initial_state()]
+    
+
+    def run_greedy_policy(self):
+        while True:
+            error = 0
+            for s in self.v_values:
+                actions = self.problem.get_available_actions(s)
+                v = self.v_values[s]
+                new_v = 0
+                aux = 0
+                for action in actions:
+                    if self.problem.is_terminal(s): 
+                        continue
+                    greedy_a = self.get_greedy_actions(s)
+                    if action in greedy_a:
+                        for prob, next_s, r in self.problem.get_transitions(state = s, action=action):
+                            aux += prob * (r + self.gamma * self.v_values[next_s])
+                    
+                        new_v +=  aux
+                        break
+                
+                self.v_values[s] = new_v
+                error = max(error, abs(v - self.v_values[s]))
+            
+            if error < self.threshold:
+                break
+
+
+    def get_greedy_actions(self, current_state):
+        actions = self.problem.get_available_actions(current_state)
+
+        action_values = {}
+        greedy_actions = []
+        for action in actions:
+            aux = 0
+
+            for prob, next_s, r in self.problem.get_transitions(state = current_state, action=action):
+                aux += prob * (r + self.gamma * self.v_values[next_s])
+            
+            action_values[action] = aux
+        max_value = max(list(action_values.values()))
+        for action in action_values:
+            if action_values[action] == max_value:
+                greedy_actions.append(action)
+
+        return greedy_actions
+
+
+
+            
+
+    
     
 
